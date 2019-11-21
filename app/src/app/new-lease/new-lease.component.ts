8e196d0b9d427a4fdd4e46c533266188403d96ee
unknown <hamzariaz1994@gmail.com>
2019-11-21 12:17:59 +0500

    errors sressolved

--- app/src/app/new-lease/new-lease.component.ts
+++ app/src/app/new-lease/new-lease.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Form } from '@angular/forms';
 import { async } from 'q';
 import axios from "axios";
-import { Alert } from 'selenium-webdriver';
-import { KeyValuePipe } from '@angular/common';
 const url = "http://compliancetool.herokuapp.com/calculation/lease";
 
 declare var $: any;
@@ -36,7 +34,82 @@ export class NewLeaseComponent implements OnInit {
     constructor() { }
 
 
-    ngOnInit() { }
+    ngOnInit() {
+
+        $(document).ready(function () {
+            var navListItems = $('div.setup-panel div a'),
+                allWells = $('.setup-content'),
+                allNextBtn = $('.nextBtn');
+    
+            allWells.hide();
+    
+            navListItems.click(function (e) {
+                e.preventDefault();
+                var $target = $($(this).attr('href')),
+                    $item = $(this);
+    
+                if (!$item.hasClass('disabled')) {
+                    navListItems.removeClass('btn-primary').addClass('btn-default');
+                    $item.addClass('btn-primary');
+                    allWells.hide();
+                    $target.show();
+                    $target.find('input:eq(0)').focus();
+                }
+            });
+    
+            allNextBtn.click(function () {
+                var curStep = $(this).closest(".setup-content"),
+                    curStepBtn = curStep.attr("id"),
+                    nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
+                    curInputs = curStep.find("input[type='text'],input[type='url']"),
+                    isValid = true;
+                debugger
+                if (curStepBtn == "step-1") {
+                    $('.new_lease1').css('display', 'block');
+                    $('.new_lease').css('display', 'none');
+                    $('.new_lease2').css('display', 'none');
+                    $('.new_lease3').css('display', 'none');
+                    $('.new_lease4').css('display', 'none');
+                }
+                if (curStepBtn == "step-2") {
+                    $('.new_lease2').css('display', 'block');
+                    $('.new_lease').css('display', 'none');
+                    $('.new_lease1').css('display', 'none');
+                    $('.new_lease3').css('display', 'none');
+                    $('.new_lease4').css('display', 'none');
+                }
+                if (curStepBtn == "step-3") {
+                    $('.new_lease3').css('display', 'block');
+                    $('.new_lease').css('display', 'none');
+                    $('.new_lease1').css('display', 'none');
+                    $('.new_lease2').css('display', 'none');
+                    $('.new_lease4').css('display', 'none');
+                }
+                if (curStepBtn == "step-4") {
+                    $('.new_lease4').css('display', 'block');
+                    $('.new_lease').css('display', 'none');
+                    $('.new_lease1').css('display', 'none');
+                    $('.new_lease3').css('display', 'none');
+                    $('.new_lease2').css('display', 'none');
+                }
+                $(".form-group").removeClass("has-error");
+                for (var i = 0; i < curInputs.length; i++) {
+                    if (!curInputs[i].validity.valid) {
+                        isValid = false;
+                        $(curInputs[i]).closest(".form-group").addClass("has-error");
+                    }
+                }
+    
+                if (isValid)
+                    debugger;
+                nextStepWizard.removeAttr('disabled').trigger('click');
+    
+    
+    
+            });
+            $('div.setup-panel div a.btn-primary').trigger('click');
+        });
+     }
 
 
 
@@ -64,7 +137,7 @@ export class NewLeaseComponent implements OnInit {
 
    
             const response = await axios.post(url, data);
-            console.log(JSON.parse(response.));
+            alert(data);
             return response.data;
             
 
@@ -73,76 +146,4 @@ export class NewLeaseComponent implements OnInit {
 
 }
 
-    $(document).ready(function () {
-        var navListItems = $('div.setup-panel div a'),
-            allWells = $('.setup-content'),
-            allNextBtn = $('.nextBtn');
-
-        allWells.hide();
-
-        navListItems.click(function (e) {
-            e.preventDefault();
-            var $target = $($(this).attr('href')),
-                $item = $(this);
-
-            if (!$item.hasClass('disabled')) {
-                navListItems.removeClass('btn-primary').addClass('btn-default');
-                $item.addClass('btn-primary');
-                allWells.hide();
-                $target.show();
-                $target.find('input:eq(0)').focus();
-            }
-        });
-
-        allNextBtn.click(function () {
-            var curStep = $(this).closest(".setup-content"),
-                curStepBtn = curStep.attr("id"),
-                nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
-                curInputs = curStep.find("input[type='text'],input[type='url']"),
-                isValid = true;
-            debugger
-            if (curStepBtn == "step-1") {
-                $('.new_lease1').css('display', 'block');
-                $('.new_lease').css('display', 'none');
-                $('.new_lease2').css('display', 'none');
-                $('.new_lease3').css('display', 'none');
-                $('.new_lease4').css('display', 'none');
-            }
-            if (curStepBtn == "step-2") {
-                $('.new_lease2').css('display', 'block');
-                $('.new_lease').css('display', 'none');
-                $('.new_lease1').css('display', 'none');
-                $('.new_lease3').css('display', 'none');
-                $('.new_lease4').css('display', 'none');
-            }
-            if (curStepBtn == "step-3") {
-                $('.new_lease3').css('display', 'block');
-                $('.new_lease').css('display', 'none');
-                $('.new_lease1').css('display', 'none');
-                $('.new_lease2').css('display', 'none');
-                $('.new_lease4').css('display', 'none');
-            }
-            if (curStepBtn == "step-4") {
-                $('.new_lease4').css('display', 'block');
-                $('.new_lease').css('display', 'none');
-                $('.new_lease1').css('display', 'none');
-                $('.new_lease3').css('display', 'none');
-                $('.new_lease2').css('display', 'none');
-            }
-            $(".form-group").removeClass("has-error");
-            for (var i = 0; i < curInputs.length; i++) {
-                if (!curInputs[i].validity.valid) {
-                    isValid = false;
-                    $(curInputs[i]).closest(".form-group").addClass("has-error");
-                }
-            }
-
-            if (isValid)
-                debugger;
-            nextStepWizard.removeAttr('disabled').trigger('click');
-
-
-
-        });
-        $('div.setup-panel div a.btn-primary').trigger('click');
-    });
+  
